dcd9a10764a1993bdc6261379db1fdee7a0db15a
Aleksandr Nikitinskikh <ya-kaze-san@yandex.ru>
2022-07-21 09:45:19 +0500

    Modernization function of coapiration js files. Made new file swiper.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,5 @@
 import * as flsFunctions from './modules/functions.js'
-import * as feedback-swiper from './modules/feedback-swiper'
-import * as popular-swiper from './IndexPage/modules/popular__swiper'
-import * as swiper-videos from './IndexPage/modules/swiper-videos'
-
+import * as swiper from './modules/Index.Page/swiper.js'
 
 flsFunctions.isWebp()
 
-import Swiper, { Navigation, Pagination } from 'swiper'
-
-const swiper = new Swiper()
